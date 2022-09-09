94425e6796c7985f7dccf2e005c11cb5667815ff
Borna Hrastovec <borna.hrastovec@ag04.com>
2022-09-09 13:10:29 +0200

    Finish Catalog API and add Docker files

---
+++ src/Services/Catalog/Catalog.Api/Data/Context/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data.Context
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
